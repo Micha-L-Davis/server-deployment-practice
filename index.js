25b3b2673d535119f0627d20ee35eeeef1a94391
Micha Davis <m.leigh.davis@gmail.com>
2022-03-21 13:24:49 -0700

    add PORT variable

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
 const app = require('./server/ap.js');
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
